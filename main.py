cf356e291fc1177549f5732fdab2f9bd5298cef9
mac <iliadi.lo.lolita@gmail.com>
2023-06-06 18:44:26 +0200

    Render the app titles

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=500)
+    st.image("gitava.png", width=400)
 
 with col2:
     st.title("Lolita Iliadi")
@@ -19,6 +20,18 @@ with col2:
     st.info(content1)
 
 content2 = '''
-Below you can find some of the apps I have built in Python. Feel free to contact me!
+Below you can find some of the apps I have built in Python. 
+Feel free to contact me!
 '''
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
