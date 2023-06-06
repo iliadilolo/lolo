edf01d86db69b609f0f3230fb1617efe607a295a
mac <iliadi.lo.lolita@gmail.com>
2023-06-06 15:04:07 +0200

    Add data source and image files

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Lolita Iliadi")
+    content = """
+    Hello there! I'm a Lola, 23 years old, I am deeply passionate 
+    about learning Python. I've embarked on a thrilling journey to master 
+    this programming language, and I've created this portfolio 
+    website as a platform to showcase my coding skills and projects.
+    
+    """
+    st.info(content)
