08e58d7037b5efe78b0cf384a67415c67a6a0ca2
mac <iliadi.lo.lolita@gmail.com>
2023-06-07 13:12:28 +0200

    Send emails

--- send_email.py
+++ send_email.py
@@ -1,19 +1,15 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "iliadi.lo.lolita@gmail.com"
-password = "voaaiorokinjpraf"
+def send_email(message):
+    global host, port, username, password
+    host = "smtp.gmail.com"
+    port = 465
+    username = "iliadi.lo.lolita@gmail.com"
+    password = "voaaiorokinjpraf"
+    receiver = "iliadi.lo.lolita@gmail.com"
+    my_context = ssl.create_default_context()
 
-receiver = "iliadi.lo.lolita@gmail.com"
-my_context = ssl.create_default_context()
-message = '''\
-Subject: It's me
-Hi!
-How are you?
-'''
-
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
